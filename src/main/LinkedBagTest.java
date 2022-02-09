ffa4b4c3f0ff4b9f75ac4b16409bb77da5f85cd5
Leonardo <leonardodavalos3@gmail.com>
2022-02-08 20:07:58 -0800

    Source Code Added

--- src/main/LinkedBagTest.java
+++ src/main/LinkedBagTest.java
@@ -5,9 +5,7 @@ public class LinkedBagTest {
 	public static void main(String[] args) {
 	
 		
-		System.out.println("This is linked bag test");
-		
-		System.out.println(LinkedBag.count);
+
 		
 		
 	}
