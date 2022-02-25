9dfc7c438df58b2a2a4ea7347a1230c4328366ee
Leonardo <leonardodavalos3@gmail.com>
2022-02-25 11:30:20 -0800

    BagDriver updated

--- src/main/BagDriver.java
+++ src/main/BagDriver.java
@@ -32,9 +32,30 @@ public class BagDriver {
 		//BagInterface<Character> ArrayDifferenceTest= arrayBag.difference(linkedBag);
 		//printer(ArrayDifferenceTest, "Array-DifferenceTest");
 		
-		BagInterface<Character> linkedBag1 = new LinkedBag<>();
-		BagInterface<Character> arrayBag1 = new ResizableArrayBag<>();
+		BagInterface<Integer> linkedBag1 = new LinkedBag<>();
+		BagInterface<Integer> arrayBag1 = new ResizableArrayBag<>();
 		
+		Integer[] count0 = { 1,2,3,4,5,6,7,8,9,10 };
+		Integer[] count1 = { 5,6,7,8,9,10,11,12,13,14,15 };
+		for (int i=0;i<count0.length;i++) {
+			linkedBag1.add(count0[i]);
+			arrayBag1.add(count1[i]);
+		}
+		
+		BagInterface<Integer> linkedUnionTest1= linkedBag1.union(arrayBag1);
+		printer(linkedUnionTest1, "LinkedBag-UnionTest");
+		//BagInterface<Character> arrayUnionTest1 = arrayBag1.union(linkedBag1);
+		//printer(arrayUnionTest1, "LinkedBag-UnionTest");
+		 
+		BagInterface<Integer> linkedIntersectTest1= linkedBag1.intersection(arrayBag1);
+		printer(linkedIntersectTest1, "LinkedBag-IntersectTest");
+		//BagInterface<Character> ArrayIntersectTest1 = arrayBag1.intersection(linkedBag1);
+		//printer(ArrayIntersectTest1, "Array-IntersectTest");
+
+		BagInterface<Integer> linkedDifferenceTest1= linkedBag1.difference(arrayBag1);
+		printer(linkedDifferenceTest1, "LinkedBag-DifferenceTest");
+		//BagInterface<Character> ArrayDifferenceTest1= arrayBag1.difference(linkedBag1);
+		//printer(ArrayDifferenceTest1, "Array-DifferenceTest");
 		
 		
 		
